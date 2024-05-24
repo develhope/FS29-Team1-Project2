77229680c5bf04eebf5e531ce5aee08a0f7cc98a
Emanuele <emanuelevutera@yahoo.it>
2024-05-24 12:01:42 +0200

    added listTask function

--- taskManagementSystem.js
+++ taskManagementSystem.js
@@ -1,1 +1,11 @@
-const task = [];
+const tasks = [];
+
+function listTask() {
+  let index = 0;
+  setTimeout(() => {
+    tasks.forEach((task) => {
+      console.log(task, index);
+      index++;
+    });
+  }, 2000);
+}
